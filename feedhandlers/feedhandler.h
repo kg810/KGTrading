e2aff328e5dd3e6207db31571c62e5cd86c01bff
fewang <fengfzy@gmail.com>
2019-06-05 23:02:00 +0800

    add CTP header files

---
+++ feedhandlers/feedhandler.h
@@ -0,0 +1,1 @@
+#include "../kglib/mmaputil.h"
--- kglib/mmap/mmaputil.h
+++ kglib/mmap/mmaputil.h
@@ -30,16 +30,23 @@ class BookContainer
 public:
     BookContainer(size_t _numOfBooks) : numOfBooks(_numOfBooks) {}
 
-    ssize_t init() try
+    ssize_t init()
     {
         // reserve space for metaHeader
         metaEntries.reserve(numOfBooks * sizeOfMetaEntry);
         metaEntries.resize(numOfBooks * sizeOfMetaEntry);
         sizeOfMeta = metaEntries.size();
 
-        shared_memory_object shm(open_or_create, "BOOKCONTAINER", read_write);
-        shm.truncate(metaEntries.size() + sizeOfBook * numOfBooks);
-        mapped_region region(shm, read_write);
+        try
+        {
+            shared_memory_object shm(open_or_create, "BOOKCONTAINER", read_write);
+            shm.truncate(metaEntries.size() + sizeOfBook * numOfBooks);
+            mapped_region region(shm, read_write);
+        }
+        catch( interprocess_exception& ex )
+        {
+            throw std::runtime_error(ex.what());
+        }
 
         // initialize meta entries
         for(int idx = 0; idx < numOfBooks; ++idx)
@@ -56,16 +63,6 @@ public:
             std::memmove(region.get_address() + sizeOfMeta + idx * sizeOfBook, &bk, sizeOfBook);
         }
     }
-    catch( interprocess_exception& ex )
-    {
-        throw std::runtime_error(ex.what());
-    }
-    catch( ... )
-    {
-        throw;
-    }
-
-
 
 private:
     std::vector<MetaEntry> metaEntries;
